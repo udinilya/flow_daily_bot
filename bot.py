d4b1ff37f895026d531fd82cb800881d460d68d2
ilya <udin76@bk.ru>
2021-11-23 13:54:39 +0300

    def daily_message def register_callback

--- bot.py
+++ bot.py
@@ -4,16 +4,23 @@ import datetime
 import pytz
 
 
-def callback_daily(context: CallbackContext):
-    context.bot.send_message(chat_id=-693040330,
-                             text=' Необходимо направить задания')
+def daily_message(context: CallbackContext):
+    context.bot.send_message(chat_id=x,
+                             text='Не забудьте написать о выполненных задачах')
+
+
+def register_callback(update: Update, context: CallbackContext):
+    global x
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text='Напоминания о статусах запущены')
+    context.job_queue.run_daily(daily_message, time=datetime.time(10, 0, tzinfo=pytz.timezone('Europe/Moscow')), days=tuple(range(0, 5)))
+    x = update.message.chat_id
 
 
 updater = Updater('2115574444:AAHL5eyZCEjkQRn4FILqYdXvhR4UJp76Ih0')
 
-
-j = updater.job_queue
-job_daily = j.run_daily(callback_daily, time=datetime.time(10, 0, tzinfo=pytz.timezone('Europe/Moscow')), days=tuple(range(0, 5)))
+register_handler = CommandHandler('register', register_callback)
+updater.dispatcher.add_handler(register_handler)
 
 updater.start_polling()
 updater.idle()
