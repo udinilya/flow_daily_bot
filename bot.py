5ad55c15f5990fa94df90bdc4c50ebe43fb392cf
ilya <udin76@bk.ru>
2021-12-10 16:33:16 +0300

    new version

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 
 
 def status_reminders_are_running(context: CallbackContext):
-    with open('flow_daily_bot/chat_list.txt', 'r', encoding='utf8') as f:
+    with open('chat_list.txt', 'r', encoding='utf8') as f:
         chat_list = f.readlines()
         for chat_id in chat_list:
             try:
@@ -19,7 +19,7 @@ def status_reminders_are_running(context: CallbackContext):
 
 
 def remind_about_status(context: CallbackContext):
-    with open('flow_daily_bot/chat_list.txt', 'r', encoding='utf8') as f:
+    with open('chat_list.txt', 'r', encoding='utf8') as f:
         chat_list = f.readlines()
         for chat_id in chat_list:
             try:
@@ -29,46 +29,93 @@ def remind_about_status(context: CallbackContext):
 
 
 def add_chat_id_in_chat_list(update: Update, context: CallbackContext):
-    with open('flow_daily_bot/chat_list.txt', 'r', encoding='utf8') as f:
-        current_chat = update.message.chat_id
+    with open(f'chat_list.txt', 'a+', encoding='utf8') as f:
+        pass
+
+    with open('chat_list.txt', 'r', encoding='utf8') as f:
         chat_list = []
         for chat_id in f:
             chat_list.append(int(chat_id.strip()))
-    if current_chat not in chat_list:
-        with open('flow_daily_bot/chat_list.txt', 'a', encoding='utf8') as f:
-            print(current_chat, file=f)
+
+    if update.message.chat_id not in chat_list:
+        with open('chat_list.txt', 'a', encoding='utf8') as f:
+            print(update.message.chat_id, file=f)
 
 
 def get_chat_members(update: Update, context: CallbackContext):
-    with open(f'flow_daily_bot/chat_members/chat_members{update.message.chat_id}.txt', 'a+', encoding='utf8') as f:
-        print(update.effective_user.name, file=f)
+    with open(f'chat_members/chat_members{update.message.chat_id}.txt', 'a+',
+            encoding='utf8') as f:
+        pass
+
+    with open(f'chat_members/chat_members{update.message.chat_id}.txt', 'r',
+                encoding='utf8') as f:
+        chat_members = []
+        for user_name in f:
+            chat_members.append(str(user_name.strip()))
+
+    if update.effective_user.name not in chat_members:
+        with open(f'chat_members/chat_members{update.message.chat_id}.txt', 'a',
+                encoding='utf8') as f:
+            print(update.effective_user.name, file=f)
 
 
 def get_responded_members(update: Update, context: CallbackContext):
-    with open(f'flow_daily_bot/responded_members/responded_members{update.message.chat_id}.txt', 'a+',
-              encoding='utf8') as f:
-        print(update.effective_user.name, file=f)
+    with open(f'responded_members/responded_members{update.message.chat_id}.txt', 'a+',
+            encoding='utf8') as f:
+        pass
+
+    with open(f'responded_members/responded_members{update.message.chat_id}.txt', 'r',
+                encoding='utf8') as f:
+        responded_members = []
+        for user_name in f:
+            responded_members.append(str(user_name.strip()))
+
+    if update.effective_user.name not in responded_members:
+        with open(f'responded_members/responded_members{update.message.chat_id}.txt', 'a',
+                    encoding='utf8') as f:
+            print(update.effective_user.name, file=f)
+
+    def send_praise_to_chat(context: CallbackContext):
+        with open(f'responded_members/responded_members{update.message.chat_id}.txt', 'r', encoding='utf8') as f:
+            responded_members = []
+            for user_name in f:
+                responded_members.append(user_name.strip())
+        number_of_responded_members = len(list(filter(None, responded_members)))
+
+        with open(f'chat_members/chat_members{update.message.chat_id}.txt', 'r', encoding='utf8') as f:
+            chat_members = []
+            for user_name in f:
+                chat_members.append(user_name.strip())
+        number_of_chat_members = len(list(filter(None, chat_members)))
+
+        if number_of_responded_members == number_of_chat_members:
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                         text='Все ответили! Молодцы!')
+
+    context.job_queue.run_daily(send_praise_to_chat, time=datetime.time(15, 48,
+                                                                            tzinfo=pytz.timezone('Europe/Moscow')),
+                                    days=tuple(range(0, 5)))
 
     def remind_about_missed_persons(context: CallbackContext):
-        with open(f'flow_daily_bot/responded_members/responded_members{update.message.chat_id}.txt', 'r',
+        with open(f'responded_members/responded_members{update.message.chat_id}.txt', 'r',
                   encoding='utf8') as f:
-            respond_list = f.readlines()
+            responded_members = f.readlines()
 
-        with open(f'flow_daily_bot/chat_members/chat_members{update.message.chat_id}.txt', 'r',
+        with open(f'chat_members/chat_members{update.message.chat_id}.txt', 'r',
                   encoding='utf8') as f:
-            chat_list = f.readlines()
+            chat_members = f.readlines()
 
-        for user in chat_list:
-            if user not in respond_list:
+        for user_name in chat_members:
+            if user_name not in responded_members:
                 context.bot.send_message(chat_id=update.message.chat_id,
-                                         text=f'Не направлен стaтус от {user}')
-    context.job_queue.run_daily(remind_about_missed_persons, time=datetime.time(12, 0,
+                                         text=f'Не направлен стaтус от {user_name}')
+    context.job_queue.run_daily(remind_about_missed_persons, time=datetime.time(15, 53,
                                 tzinfo=pytz.timezone('Europe/Moscow')), days=tuple(range(0, 5)))
 
 
 persistence = PicklePersistence(filename='persistent_storage.pkl')
 
-updater = Updater(os.getenv('TOKEN1'), persistence=persistence, use_context=True)
+updater = Updater(os.getenv('TOKEN2'), persistence=persistence, use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('register', add_chat_id_in_chat_list))
 
