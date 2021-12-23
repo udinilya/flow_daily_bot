3369f32cce42720036f0142fd6ebd354d49ab3f3
ilya <udin76@bk.ru>
2021-12-23 17:04:58 +0300

    add object_storage

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, PicklePersistence
 import telegram.error
 import os
-import bot_module
+from object_storage import ObjectStorage
 
 
 def hello(update: Update, context: CallbackContext):
@@ -13,27 +13,44 @@ def hello(update: Update, context: CallbackContext):
 
 
 def remind_about_status(context: CallbackContext):
-    with open('/home/udin76/chat_list/chat_list.txt', 'r', encoding='utf8') as f:
-        chat_list = f.readlines()
-        for chat_id in chat_list:
-            try:
-                context.bot.send_message(chat_id=chat_id, text='Не забудьте написать о выполненных задачах')
-            except telegram.error.BadRequest:
-                pass
+    chat_list = obj_storage.get('chat_list.txt', 'r')
+    for chat_id in chat_list:
+        try:
+            context.bot.send_message(chat_id=chat_id, text='Не забудьте написать о выполненных задачах')
+        except telegram.error.BadRequest:
+            pass
+
+
+def add_chat_id_in_chat_list(update: Update, context: CallbackContext):
+    try:
+        chat_list = obj_storage.get('chat_list.txt', 'r')
+        if str(update.message.chat_id) not in chat_list:
+            obj_storage.set('chat_list.txt', 'a', value=update.message.chat_id)
+
+    except FileNotFoundError:
+        with open(f'/home/udin76/storage/chat_list.txt', 'a+', encoding='utf8') as f:
+            print(update.message.chat_id, file=f)
+
+
+def get_chat_members(update: Update, context: CallbackContext):
+    try:
+        chat_members = obj_storage.get(f'chat_members{update.message.chat_id}.txt', 'r')
+        if str(update.effective_user.name) not in chat_members:
+            obj_storage.set(f'chat_members{update.message.chat_id}.txt', 'a',
+                            value=update.effective_user.name)
+
+    except FileNotFoundError:
+        with open(f'/home/udin76/storage/chat_members{update.message.chat_id}.txt', 'a+',
+                  encoding='utf8') as f:
+            print(update.effective_user.name, file=f)
 
 
 def get_responded_members(update: Update, context: CallbackContext):
     try:
-        with open(f'/home/udin76/responded_members/responded_members{update.message.chat_id}.txt', 'r',
-                  encoding='utf8') as f:
-            responded_members = []
-            for user_name in f:
-                responded_members.append(str(user_name.strip()))
-
-        if update.effective_user.name not in responded_members:
-            with open(f'/home/udin76/responded_members/responded_members{update.message.chat_id}.txt', 'a',
-                      encoding='utf8') as f:
-                print(update.effective_user.name, file=f)
+        responded_members = obj_storage_responded_members.get(f'responded_members{update.message.chat_id}.txt', 'r')
+        if str(update.effective_user.name) not in responded_members:
+            obj_storage_responded_members.set(f'responded_members{update.message.chat_id}.txt', 'a',
+                                              value=update.effective_user.name)
 
     except FileNotFoundError:
         with open(f'/home/udin76/responded_members/responded_members{update.message.chat_id}.txt', 'a+',
@@ -41,17 +58,10 @@ def get_responded_members(update: Update, context: CallbackContext):
             print(update.effective_user.name, file=f)
 
     def send_feedback_to_chat(context: CallbackContext):
-        with open(f'/home/udin76/responded_members/responded_members{update.message.chat_id}.txt', 'r',
-                  encoding='utf8') as f:
-            responded_members = []
-            for user_name in f:
-                responded_members.append(user_name.strip())
+        responded_members = obj_storage_responded_members.get(f'responded_members{update.message.chat_id}.txt', 'r')
         number_of_responded_members = len(list(filter(None, responded_members)))
 
-        with open(f'/home/udin76/chat_members/chat_members{update.message.chat_id}.txt', 'r', encoding='utf8') as f:
-            chat_members = []
-            for user_name in f:
-                chat_members.append(user_name.strip())
+        chat_members = obj_storage.get(f'chat_members{update.message.chat_id}.txt', 'r')
         number_of_chat_members = len(list(filter(None, chat_members)))
 
         if number_of_responded_members == number_of_chat_members:
@@ -64,24 +74,41 @@ def get_responded_members(update: Update, context: CallbackContext):
                                              text=f'Не направлен стaтус от {user_name}')
 
     context.job_queue.run_daily(send_feedback_to_chat, time=datetime.time(12, 0,
-                                tzinfo=pytz.timezone('Europe/Moscow')), days=tuple(range(0, 5)))
+                                                                          tzinfo=pytz.timezone('Europe/Moscow')),
+                                days=tuple(range(0, 5)))
+
+
+def create_list_of_responding_chats(context: CallbackContext):
+    with open('/home/udin76/responded_members/responding_chats.txt', 'a+', encoding='utf8') as f:
+        pass
+
+
+def add_chat_id_in_list_of_responding_chats(update: Update, context: CallbackContext):
+    try:
+        responding_chats = obj_storage_responded_members.get('responding_chats.txt', 'r')
+
+        if str(update.message.chat_id) not in responding_chats:
+            obj_storage_responded_members.set('responding_chats.txt', 'a', value=update.message.chat_id)
+
+    except FileNotFoundError:
+        with open(f'/home/udin76/responded_members/responding_chats.txt', 'a+', encoding='utf8') as f:
+            print(update.message.chat_id, file=f)
 
 
 def send_message_no_one_write_to_chat(context: CallbackContext):
-    with open('/home/udin76/responded_members/responding_chats.txt', 'r', encoding='utf8') as f:
-        responding_chats = []
-        for chat_id in f:
-            responding_chats.append(chat_id.strip())
-
-    with open('/home/udin76/chat_list/chat_list.txt', 'r', encoding='utf8') as f:
-        chat_list = []
-        for chat_id in f:
-            chat_list.append(chat_id.strip())
+    responding_chats = obj_storage_responded_members.get('responding_chats.txt', 'r')
+    chat_list = obj_storage.get('chat_list.txt', 'r')
 
     for chat_id in chat_list:
-        if chat_id not in responding_chats:
-            context.bot.send_message(chat_id=chat_id, text='Сегодня никто не направил отчет!')
+        try:
+            if chat_id not in responding_chats:
+                context.bot.send_message(chat_id=chat_id, text='Сегодня никто не направил отчет!')
+        except telegram.error.BadRequest:
+            pass
 
+
+obj_storage = ObjectStorage('/home/udin76/storage')
+obj_storage_responded_members = ObjectStorage('/home/udin76/responded_members')
 
 persistence = PicklePersistence(filename='persistent_storage.pkl')
 
@@ -91,15 +118,15 @@ with open('/home/udin76/token.txt', 'r', encoding='utf8') as f:
 updater = Updater(token, persistence=persistence, use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(CommandHandler('register', bot_module.add_chat_id_in_chat_list))
-updater.dispatcher.add_handler(CommandHandler('run', bot_module.get_chat_members))
+updater.dispatcher.add_handler(CommandHandler('register', add_chat_id_in_chat_list))
+updater.dispatcher.add_handler(CommandHandler('run', get_chat_members))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, get_responded_members))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, bot_module.add_chat_id_in_list_of_responding_chats))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, add_chat_id_in_list_of_responding_chats))
 
 updater.job_queue.run_daily(remind_about_status, time=datetime.time(10, 0, tzinfo=pytz.timezone('Europe/Moscow')),
                             days=tuple(range(0, 5)))
 
-updater.job_queue.run_daily(bot_module.create_list_of_responding_chats, time=datetime.time(5, 0,
+updater.job_queue.run_daily(create_list_of_responding_chats, time=datetime.time(5, 0,
                             tzinfo=pytz.timezone('Europe/Moscow')), days=tuple(range(0, 5)))
 
 updater.job_queue.run_daily(send_message_no_one_write_to_chat, time=datetime.time(12, 1,
